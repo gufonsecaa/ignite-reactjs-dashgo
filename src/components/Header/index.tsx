54633429b2d50055659ce7e8fbc6dbb4d9fa81b7
Gustavo Fonseca <gu.fonsecaa@gmail.com>
2021-04-17 13:02:51 -0300

    init

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,51 @@
+import { Flex, Icon, IconButton, useBreakpointValue } from '@chakra-ui/react'
+
+import { Logo } from './Logo'
+import { SearchBox } from './SearchBox'
+import { NotificationsNav } from './NotificationsNav'
+import { Profile } from './Profile'
+
+import { useSidebarDrawer } from '../../contexts/SidebarDrawerContext'
+import { RiMenuLine } from 'react-icons/ri'
+
+export function Header() {
+  const isWideVersion = useBreakpointValue({
+    base: false,
+    lg: true
+  })
+
+  const { onOpen } = useSidebarDrawer()
+
+  return (
+    <Flex
+      as="header"
+      w="100%"
+      maxWidth={1480}
+      h="20"
+      mx="auto"
+      mt="4"
+      px="6"
+      align="center"
+    >
+      { !isWideVersion && (
+        <IconButton
+          aria-label="Open navigation"
+          icon={<Icon as={RiMenuLine} />}
+          fontSize="24"
+          variant="unstyled"
+          mr="2"
+          onClick={onOpen}
+        />
+      ) }
+
+      <Logo />
+
+      {isWideVersion && <SearchBox />}
+
+      <Flex align="center" ml="auto">
+        <NotificationsNav />
+        <Profile showProfileData={isWideVersion} />
+      </Flex>
+    </Flex>
+  )
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,76 @@
+import { Flex, Button, Stack } from '@chakra-ui/react'
+import { SubmitHandler, useForm } from 'react-hook-form'
+import * as yup from 'yup'
+import { yupResolver } from '@hookform/resolvers/yup'
+
+import { Input } from '../components/Form/Input'
+
+type SignInFormData = {
+  email: string;
+  password: string
+}
+
+const signInFormSchema = yup.object().shape({
+  email: yup.string().required('E-mail obrigatório.').email('E-mail inválido.'),
+  password: yup.string().required('Senha obrigatória')
+})
+
+export default function SignIn() {
+  const { register, handleSubmit, formState, errors } = useForm({
+    resolver: yupResolver(signInFormSchema)
+  })
+
+  const handleSignIn: SubmitHandler<SignInFormData> = async (values) => {
+    await new Promise(resolve => setTimeout(resolve, 2000))
+
+    console.log(values)
+  }
+
+  return (
+    <Flex
+      w="100vw"
+      h="100vh"
+      align="center"
+      justify="center"
+    >
+      <Flex
+        as="form"
+        flexDir="column"
+        w="100%"
+        maxWidth={360}
+        bg="gray.800"
+        p="8"
+        borderRadius={8}
+        onSubmit={handleSubmit(handleSignIn)}
+      >
+        <Stack spacing="4">
+          <Input
+            type="email"
+            name="email"
+            label="E-mail"
+            ref={register}
+            error={errors.email}
+          />
+
+          <Input
+            type="password"
+            name="password"
+            label="Senha"
+            ref={register}
+            error={errors.password}
+          />
+        </Stack>
+
+        <Button
+          type="submit"
+          mt="6"
+          colorScheme="pink"
+          size="lg"
+          isLoading={formState.isSubmitting}
+        >
+          Entrar
+        </Button>
+      </Flex>
+    </Flex>
+  )
+}
